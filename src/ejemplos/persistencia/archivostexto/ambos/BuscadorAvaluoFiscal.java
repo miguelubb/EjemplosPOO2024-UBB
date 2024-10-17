c0c059d261b49587be0909974f221c90917efe73
Miguel Romero Vásquez <miguelubb@gmail.com>
2024-10-17 10:55:49 -0300

    se agrega explicación a ejercicio de archivo

--- src/ejemplos/persistencia/archivostexto/ambos/BuscadorAvaluoFiscal.java
+++ src/ejemplos/persistencia/archivostexto/ambos/BuscadorAvaluoFiscal.java
@@ -1,4 +1,9 @@
 package ejemplos.persistencia.archivostexto.ambos;
 
 public class BuscadorAvaluoFiscal {
+    public static void main(String[] args) {
+        //menu
+        // busqueda por año, marca, modelo
+        // busqueda por rango tazación (min, max)
+    }
 }
